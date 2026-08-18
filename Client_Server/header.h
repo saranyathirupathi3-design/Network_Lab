52aab315dc683f34eb8420bba0ce5d3a4541faa0
Sharanya T <saranyathirupathi3@gmail.com>
2026-08-18 23:54:42 +0530

    Add header.h with socket and protocol definitions

---
+++ Client_Server/header.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define SERV_PORT 8080
+#define MAX 100
+
+
+#define FLAG "01111110"
+#define ESCAPE "01111101"
+
+#endif
